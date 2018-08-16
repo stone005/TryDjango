5fe53818ff783dc81b76450035405fdcaa093ca5
Nadir GIBIN <39050790+stone005@users.noreply.github.com>
2018-08-16 09:42:28 +0200

    Add files via upload

---
+++ src/pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	# return HttpResponse('<h1>Hello World</h1>')
+	return render(request, 'home.html', {})
+
+def contact_view(request, *args, **kwargs):
+	# return HttpResponse('<h1>Contact page</h1>')
+	return render(request, 'contact.html', {})
+
+def about_view(request, *args, **kwargs):
+	# return HttpResponse('<h1>About page</h1>')
+	return render(request, 'about.html', {})
